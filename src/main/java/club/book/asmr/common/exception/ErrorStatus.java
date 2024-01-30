b8a6b77259c13bb56610e516ffc984c6ea1e1a35
yebin <dev.devin.kr@gmail.com>
2024-01-30 22:32:40 +0900

    feat(#5): 커스텀 예외 클래스 생성

---
+++ src/main/java/club/book/asmr/common/exception/ErrorStatus.java
@@ -0,0 +1,43 @@
+package club.book.asmr.common.exception;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ErrorStatus {
+    /**
+     * 400 BAD REQUEST
+     */
+    REQUEST_VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "잘못된 요청입니다"),
+    NOT_SUPPORTED_OAUTH_PROVIDER(HttpStatus.BAD_REQUEST, "지원하지 않는 OAuth 제공자입니다."),
+    CANNOT_GET_DATA_FROM_ALADIN(HttpStatus.BAD_REQUEST, "알라딘 Open Api에서 데이터를 가져올 수 없습니다."),
+
+    /**
+     * 401 UNAUTHORIZED
+     */
+    EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 엑세스 토큰입니다."),
+    CANNOT_FOUND_USER(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다."),
+
+    /**
+     * 404 NOT FOUND
+     */
+    NOT_FOUND_USER_EXCEPTION(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다"),
+    NOT_FOUND_TOKEN_EXCEPTION(HttpStatus.NOT_FOUND, "토큰을 찾을 수 없습니다."),
+
+    /**
+     * 409 CONFLICT
+     */
+    ALREADY_EXIST_USER_EXCEPTION(HttpStatus.CONFLICT, "이미 존재하는 유저입니다"),
+
+    /**
+     * 500 INTERNAL SERVER ERROR
+     */
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 오류가 발생했습니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
