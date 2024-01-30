77387059a97b82016da6366fba2c9a0deb736b36
yebin <dev.devin.kr@gmail.com>
2024-01-30 22:41:55 +0900

    fix(#5): 도서 테이블 컬럼 사이즈 문제 해결

--- src/main/java/club/book/asmr/book/entity/Book.java
+++ src/main/java/club/book/asmr/book/entity/Book.java
@@ -1,39 +1,47 @@
 package club.book.asmr.book.entity;
 
 import club.book.asmr.common.entity.BaseTime;
-import java.sql.Timestamp;
+import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@ToString
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
+@Table
 public class Book extends BaseTime {
     @Id
     private String isbn;
-    @Column(nullable = false)
+    @Column(nullable = false, length = 200)
     private String title;
     @Column(nullable = false)
     private Integer itemPage;
-    @Column
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String description;
-    @Column
+    @Column(nullable = false, length = 100)
     private String frontCoverImageUrl;
-    @Column
+    @Column(length = 100)
     private String sideCoverImageUrl;
-    @Column
+    @Column(nullable = false)
     private Integer sizeDepth;
-    @Column
+    @Column(nullable = false)
     private Integer sizeWidth;
-    @Column
+    @Column(nullable = false)
     private Integer sizeHeight;
-    @Column
-    private Timestamp publishedAt;
-    @Column
+    @Column(nullable = false)
+    private LocalDate publishedDate;
+    @Column(nullable = false, length = 200)
     private String author;
-    @Column
+    @Column(nullable = false)
     private String publisher;
 }
