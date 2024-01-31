566028b58b7d1d9ce79680cd36a37a047d4626ab
yebin <dev.devin.kr@gmail.com>
2024-01-31 21:44:17 +0900

    fix(#5): 책 검색 api 응답 예외 처리

--- src/main/java/club/book/asmr/book/controller/BookController.java
+++ src/main/java/club/book/asmr/book/controller/BookController.java
@@ -1,6 +1,8 @@
 package club.book.asmr.book.controller;
 
+import club.book.asmr.book.dto.BookSearchItemResponseDto;
 import club.book.asmr.book.service.BookInfoService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,17 @@ public class BookController {
 
     @GetMapping("/api/v1/book")
     public ResponseEntity<?> searchBook(@RequestParam(name = "keyword") String keyword) {
-        return ResponseEntity.ok(bookInfoService.searchBookByKeyword(keyword));
+        try {
+            List<BookSearchItemResponseDto> responseDtos = bookInfoService.searchBookByKeyword(keyword);
+            if (responseDtos.isEmpty()) {
+                return ResponseEntity.noContent().build();
+            }
+            return ResponseEntity.ok(responseDtos);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
+
+    // TODO 도서 직접 추가 isbn13
+    // TODO 도서 상세 검색
 }
